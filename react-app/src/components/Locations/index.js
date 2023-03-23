ccf839121d8dd08438726183acbfb9284e6dec8c
anton <antonhdo@gmail.com>
2023-03-23 10:20:05 -0700

    Got stuff to show up on the page

--- react-app/src/components/Locations/index.js
+++ react-app/src/components/Locations/index.js
@@ -5,14 +5,56 @@ import { fetchLocations } from "../../store/locations";
 
 const Locations = () => {
   const dispatch = useDispatch();
-  const locations = useSelector(state => state.locations)
-  console.log("locations from home page", locations)
+  const locations = useSelector(state => state.locations.locations)
+  const user = useSelector((state) => state.session.user)
+
+  const userLoggedIn = () => {
+    if (user) {
+      return user && (
+        <div></div>
+      )
+    } else {
+      return (
+        <div className="loggedOutHomeBannerContainer">
+          <h2>Make the first move!</h2>
+          <div>Start meeting new people in your area! If you already have an account, sign in to use Bumble on the web.</div>
+          <div>
+            <button>Modal Join</button>
+            <button>Modal Sign in</button>
+          </div>
+        </div>
+      )
+    }
+  }
+
+  // console.log("locations from home page", locations)
   useEffect(() => {
     dispatch(fetchLocations())
   }, [dispatch])
 
-  return (
-    <div>Sup</div>
+  if (!locations) {
+    return <div>Loading...</div>
+  }
+
+  const locationsArr = Object.values(locations)
+  console.log("from home page", locationsArr)
+
+  return locations && locationsArr && (
+    <div className="homePageContainer">
+      {userLoggedIn()}
+      <div className="homeLocationsContainer">
+        {locationsArr?.map((location) => (
+          <NavLink key={location.id} to={`/locations/${location.id}`}>
+            <img src={location.preview_img} />
+            <div>{location.location_name}</div>
+            <div className="numberOfPeopleAndIcon">
+              <div>{location?.users?.length}</div>
+              <div><i className="fa-solid fa-user-group"></i></div>
+            </div>
+          </NavLink>
+        ))}
+      </div>
+    </div>
   )
 }
 
