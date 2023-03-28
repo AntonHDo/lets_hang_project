df06a63ea0e61114d5903da983324c61a7c807e8
anton <antonhdo@gmail.com>
2023-03-28 16:24:15 -0700

    Done with edit?

--- react-app/src/components/CurrentUser/EditSchedulingModal.js
+++ react-app/src/components/CurrentUser/EditSchedulingModal.js
@@ -2,15 +2,85 @@ import { useModal } from "../../context/Modal";
 import React, { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux";
 import { updateScheduling } from "../../store/schedulings";
+import { fetchSchedulings } from "../../store/schedulings";
+
+
 
 const EditSchedulingModal = ({ schedule, onDeleted }) => {
   const dispatch = useDispatch();
+  const allSchedulings = useSelector(state => state.schedulings.schedulings)
   const { closeModal } = useModal()
   const [date, setDate] = useState(schedule.date)
   const currentUser = useSelector((state) => state.session.user)
   const [timeStart, setTimeStart] = useState(schedule.time_start)
   const [timeEnd, setTimeEnd] = useState(schedule.time_end)
   const [status, setStatus] = useState("accepted")
+
+  const [errors, setErrors] = useState({
+    date: "",
+    timeStart: "",
+    timeEnd: ""
+  })
+
+
+  const isFormValid = date && timeStart && timeEnd;
+
+  useEffect(() => {
+    dispatch(fetchSchedulings())
+  }, [dispatch])
+
+  const validateScheduling = (newScheduling) => {
+    const errors = {
+      date: "",
+      timeStart: "",
+      timeEnd: "",
+    };
+    const schedulingsArr = Object.values(allSchedulings)
+    const now = new Date();
+    const schedulingDate = new Date(newScheduling.date);
+    const startTime = new Date(`${newScheduling.date} ${newScheduling.time_start}`)
+    const endTime = new Date(`${newScheduling.date} ${newScheduling.time_end}`)
+
+    if (schedulingDate < now) {
+      errors.date = "Cannot schedule a hangout in the past";
+    }
+
+    if (startTime.getHours() < 6) {
+      errors.timeStart = "Cannot schedule a start time before 6 AM";
+    }
+
+    if (endTime.getHours() >= 23) {
+      errors.timeEnd = ("Cannot schedule an end time after 11 PM");
+    }
+    if (endTime <= startTime) {
+      errors.timeEnd = ("End time must be after the start time");
+    }
+
+
+    const conflictingSchedulings = schedulingsArr.filter((scheduling) => {
+      const existingStartTime = new Date(`${scheduling.date} ${scheduling.time_start}`);
+      const existingEndTime = new Date(`${scheduling.date} ${scheduling.time_end}`);
+      const userIds = [currentUser.id, scheduling.friend_id];
+
+      return (
+        userIds.includes(scheduling.user_id) &&
+        userIds.includes(scheduling.friend_id) &&
+        schedulingDate.toDateString() === new Date(scheduling.date).toDateString() &&
+        (
+          (startTime >= existingStartTime && startTime < existingEndTime) ||
+          (endTime > existingStartTime && endTime <= existingEndTime) ||
+          (startTime <= existingStartTime && endTime >= existingEndTime)
+        )
+      );
+    });
+    if (conflictingSchedulings.length > 0) {
+      errors.conflict = "Cannot schedule a hangout that conflicts with an existing scheduling"
+    }
+    return errors;
+  }
+
+
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -24,7 +94,17 @@ const EditSchedulingModal = ({ schedule, onDeleted }) => {
       "id": schedule.id,
       "status": status
     };
-    console.log('new scheduling', newScheduling)
+
+    const validationErrors = validateScheduling(newScheduling)
+    setErrors(validationErrors)
+
+    const hasErrors = Object.values(validationErrors).some((error) => error !== "");
+
+    if (hasErrors) {
+      console.log(errors)
+      return
+    }
+
     await dispatch(updateScheduling(newScheduling))
     closeModal()
     onDeleted((prev) => !prev)
@@ -34,6 +114,16 @@ const EditSchedulingModal = ({ schedule, onDeleted }) => {
     <div className="editSchedulingContainer">
       <h2>Edit Hang Out Times</h2>
       <form onSubmit={handleSubmit}>
+        {Object.values(errors).map((value, index) => {
+          if (value) {
+            return (
+              <div key={index} className="error-message">
+                {value}
+              </div>
+            );
+          }
+          return null;
+        })}
         <label htmlFor="date">Date:</label>
         <input
           type="date"
@@ -55,7 +145,7 @@ const EditSchedulingModal = ({ schedule, onDeleted }) => {
           value={timeEnd}
           onChange={(e) => setTimeEnd(e.target.value)}
         />
-        <button type="submit">Update Hangout!</button>
+        <button disabled={!isFormValid} type="submit">Update Hangout!</button>
         <button type="button" onClick={() => closeModal()}>Cancel</button>
       </form>
     </div>
