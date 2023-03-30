1e6163f6d05b4916baf751438d1e9dcf9e60dab7
anton <antonhdo@gmail.com>
2023-03-30 15:56:01 -0700

    Hmm still debugging

--- react-app/src/components/CurrentUser/DeleteSchedulingModal.js
+++ react-app/src/components/CurrentUser/DeleteSchedulingModal.js
@@ -6,12 +6,12 @@ import { removeScheduling } from "../../store/schedulings";
 const DeleteSchedulingModal = ({ scheduleId, onDeleted }) => {
   const dispatch = useDispatch()
   const { closeModal } = useModal()
-
+  console.log("schedule id", scheduleId)
 
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    await dispatch(removeScheduling(+scheduleId))
+    await dispatch(removeScheduling(scheduleId))
     closeModal()
     onDeleted((prev) => !prev)
   }
