77fda581fa1b0445c428bfc1666b9e6f7d13b37d
anton <antonhdo@gmail.com>
2023-03-30 19:47:23 -0700

    Lets try again

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app.models import Scheduling, Notification
+from flask import current_app
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -26,18 +26,24 @@ class User(db.Model, UserMixin):
 
     def delete_user(self):
         # Delete related schedulings
-        schedulings_to_delete = Scheduling.query.filter((Scheduling.user_id == self.id) | (Scheduling.friend_id == self.id)).all()
-        for scheduling in schedulings_to_delete:
+        schedulings = current_app.models.Scheduling.query.filter(
+            (current_app.models.Scheduling.user_id == self.id) | (current_app.models.Scheduling.friend_id == self.id)
+        ).all()
+        for scheduling in schedulings:
             scheduling.delete_scheduling()
 
         # Delete related notifications
-        notifications_to_delete = Notification.query.filter((Notification.user_id == self.id) | (Notification.other_user_id == self.id)).all()
-        for notification in notifications_to_delete:
+        notifications = current_app.models.Notification.query.filter(
+            (current_app.models.Notification.user_id == self.id) | (current_app.models.Notification.other_user_id == self.id)
+        ).all()
+        for notification in notifications:
             db.session.delete(notification)
 
         # Delete the user itself
         db.session.delete(self)
         db.session.commit()
+
+
     @property
     def password(self):
         return self.hashed_password
