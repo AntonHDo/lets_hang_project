a0fbd14e99a043730f6cea22f1b357e281284fd7
anton <antonhdo@gmail.com>
2023-03-30 18:40:54 -0700

    stashed all my changes

--- app/models/user.py
+++ app/models/user.py
@@ -1,60 +1,3 @@
-# from .db import db, environment, SCHEMA, add_prefix_for_prod
-# from werkzeug.security import generate_password_hash, check_password_hash
-# from flask_login import UserMixin
-
-
-# class User(db.Model, UserMixin):
-#     __tablename__ = 'users'
-
-#     if environment == "production":
-#         __table_args__ = {'schema': SCHEMA}
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(40), nullable=False, unique=True)
-#     email = db.Column(db.String(255), nullable=False, unique=True)
-#     hashed_password = db.Column(db.String(255), nullable=False)
-#     first_name = db.Column(db.String(255), nullable=False)
-#     last_name = db.Column(db.String(255), nullable=False)
-#     location_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("locations.id")), nullable=False)
-#     profile_picture = db.Column(db.String)
-#     date_of_birth = db.Column(db.String, nullable=False)
-#     gender = db.Column(db.String, nullable=False)
-#     about_me = db.Column(db.String(2000), nullable=False)
-
-#     #relationships
-#     location = db.relationship("Location", back_populates="users", cascade='all, delete')
-
-#     created_schedulings = db.relationship("Scheduling", backref="creator", cascade='all, delete', lazy=True, foreign_keys='Scheduling.user_id')
-
-#     received_schedulings = db.relationship("Scheduling", backref="receiver", cascade='all, delete', lazy=True, foreign_keys='Scheduling.friend_id')
-
-#     notifications = db.relationship("Notification", backref="recipient", cascade='all, delete', lazy=True, foreign_keys='Notification.user_id')
-
-#     @property
-#     def password(self):
-#         return self.hashed_password
-
-#     @password.setter
-#     def password(self, password):
-#         self.hashed_password = generate_password_hash(password)
-
-#     def check_password(self, password):
-#         return check_password_hash(self.password, password)
-
-#     def to_dict(self):
-#         return {
-#             'id': self.id,
-#             'username': self.username,
-#             'email': self.email,
-#             'first_name': self.first_name,
-#             'last_name': self.last_name,
-#             'location_id': self.location_id,
-#             'profile_picture': self.profile_picture,
-#             'date_of_birth': self.date_of_birth,
-#             'gender': self.gender,
-#             'about_me': self.about_me,
-
-#         }
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -79,13 +22,7 @@ class User(db.Model, UserMixin):
     about_me = db.Column(db.String(2000), nullable=False)
 
     #relationships
-    location = db.relationship("Location", back_populates="users", cascade='all, delete')
-
-    created_schedulings = db.relationship("Scheduling", backref="creator", cascade='all, delete', lazy=True, foreign_keys='Scheduling.user_id')
-
-    received_schedulings = db.relationship("Scheduling", backref="receiver", cascade='all, delete', lazy=True, foreign_keys='Scheduling.friend_id')
-
-    notifications_received = db.relationship("Notification", back_populates="recipient", cascade='all, delete', lazy=True, foreign_keys='Notification.user_id')
+    location = db.relationship("Location", back_populates="users")
 
     @property
     def password(self):
@@ -109,6 +46,5 @@ class User(db.Model, UserMixin):
             'profile_picture': self.profile_picture,
             'date_of_birth': self.date_of_birth,
             'gender': self.gender,
-            'about_me': self.about_me,
-
+            'about_me': self.about_me
         }
